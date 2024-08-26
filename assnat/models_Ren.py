8d7cc301f6fc7cf56a186103d89457761eba8c98
Renaud Pelissier <renaud@dalae.fr>
2024-08-26 12:54:32 +0200

    mini model and model

--- assnat/models_Ren.py
+++ assnat/models_Ren.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, SpatialDropout1D
 
 
-def embedding_lstm( X_train, X_test, y_train, y_test):
+def mini_embedding_lstm( X_train, X_test, y_train, y_test):
     X_train, X_test, y_train, y_test=sample(1000, X_train, X_test, y_train, y_test)
 
     X_train, X_test, vocab_size = tokenize_X(X_train, X_test, max_words=100)
@@ -20,5 +20,20 @@ def embedding_lstm( X_train, X_test, y_train, y_test):
 
     return model,  X_train, X_test, y_train, y_test
 
+def embedding_lstm( X_train, X_test, y_train, y_test):
+    X_train, X_test, vocab_size = tokenize_X(X_train, X_test, max_words=100000)
 
+    model = Sequential()
+    #Embedding.input_dim	Size of the vocabulary, i.e. maximum integer index + 1.
+    #Embedding.output_dim	Dimension of the dense embedding.
+    model.add(Embedding(input_dim=vocab_size+1, output_dim=100))
+    model.add(SpatialDropout1D(0.2))
+    model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(len(np.unique(y_train)), activation='softmax'))
+
+    return model,  X_train, X_test, y_train, y_test
+
+
+#fit_predict(mini_embedding_lstm, 'leg15', 5, 10, 64)
 fit_predict(embedding_lstm, 'leg15', 5, 10, 64)
