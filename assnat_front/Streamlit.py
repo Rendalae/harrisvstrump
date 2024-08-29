9d00859b988599d9104b1743df68cf51b97d9037
Renaud Pelissier <renaud@dalae.fr>
2024-08-29 19:01:28 +0200

    Fix front with png baneer

--- assnat_front/Streamlit.py
+++ assnat_front/Streamlit.py
@@ -19,6 +19,8 @@ mapping_political_people = {
     "Gauche": ["François Hollande", "Melenchon", "Arnaud Montebourg"]
 }
 
+img_background_url="assnat_front/Img_assnat.png"
+st.image(img_background_url, caption=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 
 print(f"API_URL: {API_URL}")
@@ -32,6 +34,22 @@ def get_political_group_prediction(params):
     else:
         st.error(f"Error {response.status_code}: {response.text}")
         return {}
+
+def plot_probability_chart(proba_dict):
+    groups = list(proba_dict.keys())
+    probabilities = list(proba_dict.values())
+
+    fig, ax = plt.subplots(figsize=(5, 4))
+    ax.barh(groups, probabilities, color=['orange', 'blue', 'red'])
+
+    ax.set_xlim(0, 1)  # Probability is between 0 and 1
+    ax.set_xlabel('Probability')
+    ax.set_title('Predicted Probabilities by Group')
+
+    for i in range(len(groups)):
+        ax.text(probabilities[i] + 0.01, i, f'{probabilities[i]:.2f}', va='center')
+
+    st.pyplot(fig)
 
 def validate_input(input_text):
     if len(input_text.split()) <= 5:
@@ -48,16 +66,18 @@ def display_gif(predicted_class):
         if response.status_code == 200:
             data = response.json()
             gif_url = data['data']['images']['original']['url']
-            st.image(gif_url, caption=f"Hello little {gif_topic}! (it will not lower your taxi fare, but might make you smile)")
+            st.image(gif_url, caption=f"credit giphy - random image of '{gif_topic}'")
         else:
             st.error("Failed to fetch GIF. Please try again.")
 # Streamlit interface
-st.title("Political Group Prediction")
-st.write("Enter text and theme to get a political group prediction.")
+
+
+st.title("French Political 'Who said what'")
+st.write("Enter a sentence along with its theme.\nYou will receive a prediction of which political group in the French National Assembly might say it.")
 
 # User input fields
-user_input_text = st.text_area("Enter your text here:")
-user_input_theme = st.text_input("Enter the theme here:")
+user_input_text = st.text_area("Sentence")
+user_input_theme = st.text_input("Theme")
 
 
 # Step 2: Use st.text_input to get the user input
@@ -81,5 +101,8 @@ if st.button("Predict"):
 
                 # Display a GIF (you need to have a GIF stored locally or provide a URL)
                 display_gif(predicted_class)
+
+                # Display the bar chart in the Streamlit app
+                plot_probability_chart(predictions)
         else:
             st.write("Please enter some text.")
